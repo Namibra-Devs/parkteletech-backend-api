553a767ad42e1195acb5fd3e6ce2b44120f99af8
Benson Osei Mensah <kbenson643@gmail.com>
2024-09-01 20:46:28 +0000

    add Salary model and migration
    Signed-off-by: Benson Osei Mensah <kbenson643@gmail.com>

---
+++ database/migrations/2024_09_01_204517_create_salaries_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('salaries', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('salaries');
+    }
+}
